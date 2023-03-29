cdc7c4a7cb508c41d61d3c7e840d4d48fd95928b
Rhonda Guillory <guilloryrt.ps@gmail.com>
2023-03-29 00:09:19 -0500

    init commit

--- PalindromeExercise/PalindromeExercise/Program.cs
+++ PalindromeExercise/PalindromeExercise/Program.cs
@@ -7,6 +7,9 @@ namespace PalindromeExercise
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var word = new WordSmith();
+
+            Console.WriteLine(word.IsAPalindrome("tacocat"));
         }
     }
 }
---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var len = word.Length;
+            
+            for (int i = 0; i < len/2; i++)
+            {
+                if (word[i] != word[len - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
